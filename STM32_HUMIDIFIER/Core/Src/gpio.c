548f0f55ddeedf173ad7dee7451fa95b652ab519
LiubomirBezgan <bezganliubomir@gmail.com>
2023-08-31 21:09:33 +0200

    Switch debouncing
    Switch debouncing has been ultimately ensured by hardware and "GPIO Pull-up/Pull-down" parameter modification.
    "GPIO Pull-up/Pull-down" parameter has been changed to "No pull-up and no pull-down".

--- STM32_HUMIDIFIER/Core/Src/gpio.c
+++ STM32_HUMIDIFIER/Core/Src/gpio.c
@@ -66,7 +66,7 @@ void MX_GPIO_Init(void)
   /*Configure GPIO pin : PtPin */
   GPIO_InitStruct.Pin = Joystick_button_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(Joystick_button_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PtPin */
